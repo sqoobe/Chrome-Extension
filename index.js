bcfb81117a256ff597599aae03265f5b6c81d8ca
sqoobe <sqoobe96@gmail.com>
2024-11-27 14:53:02 +0100

    add the <a> tag

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const ulEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 })
 
 function renderLeads(){
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-         listItems += "<li>" + myLeads[i] + "</li>" 
+         listItems += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+
     }
     ulEl.innerHTML = listItems
 }
