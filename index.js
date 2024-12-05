b40494ae6f80d813fb2b855d0c27df4dc6a36fb2
sqoobe <sqoobe96@gmail.com>
2024-12-05 11:11:47 +0100

    v1

--- index.js
+++ index.js
@@ -47,33 +47,3 @@ inputBtn.addEventListener("click", function() {
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
 })
-
-
-
-// function renderLeads(){
-//     let listItems = ""
-//     for (let i = 0; i < myLeads.length; i++) {
-//          //listItems += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-//          listItems += `
-//          <li>
-//             <a target='_blank' href='${myLeads[i]}'>
-//             ${myLeads[i]}
-//          </li>
-//          `
-
-//     }
-//     ulEl.innerHTML = listItems
-// }
-
-
-
-
-
-
-
-
-
-    // -createElement&make li textContent for myleads without innerhtml
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
