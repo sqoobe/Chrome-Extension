c87ad9e604f5c56566ae4f2967c3c01d854fcced
sqoobe <sqoobe96@gmail.com>
2024-12-05 10:43:29 +0100

    create the tab btn

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 //variables
 let myLeads = []
+
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
 const deleteBtn = document.querySelector("#delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.querySelector("#tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click", function(){
+    console.log(tabs[0].url)
+})
+
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
 
 deleteBtn.addEventListener("dblclick", function(){
     console.log("double clicked!")
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
 
 
@@ -25,24 +50,24 @@ inputBtn.addEventListener("click", function(){
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     
-    renderLeads()
+    render(myLeads)
     
 })
 
-function renderLeads(){
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-         //listItems += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-         listItems += `
-         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-            ${myLeads[i]}
-         </li>
-         `
+// function renderLeads(){
+//     let listItems = ""
+//     for (let i = 0; i < myLeads.length; i++) {
+//          //listItems += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+//          listItems += `
+//          <li>
+//             <a target='_blank' href='${myLeads[i]}'>
+//             ${myLeads[i]}
+//          </li>
+//          `
 
-    }
-    ulEl.innerHTML = listItems
-}
+//     }
+//     ulEl.innerHTML = listItems
+// }
 
 
 
