6e83a7170f1eca63cb4c60d1eddc5a38fb9218f7
sqoobe <sqoobe96@gmail.com>
2024-12-05 10:21:39 +0100

    delete button added

--- index.js
+++ index.js
@@ -3,18 +3,42 @@ let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
+const deleteBtn = document.querySelector("#delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function(){
+    console.log("double clicked!")
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
+
 
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    
     renderLeads()
+    
 })
 
 function renderLeads(){
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-         listItems += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+         //listItems += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+         listItems += `
+         <li>
+            <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}
+         </li>
+         `
 
     }
     ulEl.innerHTML = listItems
